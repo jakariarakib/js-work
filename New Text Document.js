cf94fc9f72c01886ab0f2a6cd07b3dbe0d53655f
jakariarakib <p28056210@gmail.com>
2025-10-17 10:25:01 +0600

    Initial commit

---
+++ New Text Document.js
@@ -0,0 +1,17 @@
+// Variables to control loop counters and character output
+var x, y, chr;
+
+// Outer loop for rows
+for (x = 1; x <= 6; x++) {
+    // Inner loop for characters in each row
+    for (y = 1; y < x; y++) {
+        // Accumulate asterisks in the 'chr' variable
+        chr = chr + ("*");
+    }
+
+    // Print the accumulated characters for the current row
+    console.log(chr);
+
+    // Reset 'chr' for the next row
+    chr = '';
+} 
